25adb1d57d2a227badae2d50f1923a21b45b6126
Jared Serrao <jared.serrao5@gmail.com>
2024-06-02 12:11:59 -1000

    test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,16 +12,16 @@ const config: Config = {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
       colors: {
-        dark: '#2F2522',
+        dark: '#915D52',
         light: '#FEFFEA',
         brown: '#A97160',
         lightGreen: '#6C896E',
         darkGreen: '#4B7F52',
       },
       fontFamily: {
-        sans: ['Noto Sans', 'sans-serif'],
-        serif: ['Noto Serif', 'serif'],
-        cursive: ['Island Moments', 'cursive']
+        sans: ['Monospace', 'Roboto'],
+        serif: ['Roboto', 'Roboto'],
+        cursive: ['Roboto']
       },
     },
     keyframes: {
